26b87a78c45ce6ecc95059bf9ac50cb545c51b12
Erik Hommel <hommel@ee-atwork.nl>
2016-12-07 10:38:57 +0100

    add SetShowExpert action

---
+++ CRM/Pumcivirules/CiviRulesActions/SetShowExpert.mgd.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  0 =>
+    array (
+      'name' => 'Pum:Civirules:Action.SetShowExpert',
+      'entity' => 'CiviRuleAction',
+      'params' =>
+        array (
+          'version' => 3,
+          'name' => 'SetShowExpert',
+          'label' => 'Set Show Expert to Customer',
+          'class_name' => 'CRM_Pumcivirules_CiviRulesActions_SetShowExpert',
+          'is_active' => 1
+        ),
+    ),
+);
